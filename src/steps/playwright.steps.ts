bb6394023c1288546cd6ca94313a1837f852a98f
tallyb <tally.barak@gmail.com>
2022-02-20 13:12:23 +0200

    fix(steps): change steps to lcoators

--- src/steps/playwright.steps.ts
+++ src/steps/playwright.steps.ts
@@ -4,9 +4,9 @@ import { Given, When, Then } from '@cucumber/cucumber';
 import expect from 'expect';
 
 Given('Go to the playwright website', async function (this: ICustomWorld) {
-  const { page } = this;
-  await page?.goto(config.BASE_URL);
-  await page?.waitForSelector('nav >> a >> text="Playwright"');
+  const page = this.page!;
+  await page.goto(config.BASE_URL);
+  await page.locator('nav >> a >> text="Playwright"').waitFor();
 });
 
 When('Change theme to {string} mode', async function (this: ICustomWorld, mode: string) {
@@ -14,13 +14,13 @@ When('Change theme to {string} mode', async function (this: ICustomWorld, mode: 
   const page = this.page!;
   const current = await page.getAttribute('html', 'data-theme');
   if (current !== mode) {
-    await page.click('nav >> div:has(input[type="checkbox"]) >> div[role="button"]');
+    await page.locator('nav >> div:has(input[type="checkbox"]) >> div[role="button"]').click();
   }
   await page.waitForSelector(`html[data-theme=${mode}]`);
 });
 
 Then('We see {string} mode', async function (this: ICustomWorld, mode: string) {
-  const { page } = this;
-  const theme = await page?.getAttribute('html', 'data-theme');
+  const page = this.page!;
+  const theme = await page.locator('html').getAttribute('data-theme');
   expect(theme).toEqual(mode);
 });
